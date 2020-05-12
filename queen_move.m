771f5d4d025e67d93322efd6ed58acd88431d218
pavlov061356 <64154579+pavlov061356@users.noreply.github.com>
2020-05-12 15:50:54 +0300

    Add files via upload

---
+++ queen_move.m
@@ -0,0 +1,53 @@
+function [ X,Y ] = queen_move( table, pos )
+
+    %Начинаем с обычных ходов, по аналогии x --- потенциальные ходы без атак, а y --- ходы с атаками%
+    i =1;
+    while table(pos(1)-i,pos(2)-i) == 0
+        X = [X,[pos(1)-i,pos(2)-i]];
+        i=i+1;
+    end
+    y = square_check(table, [pos(1)-i,pos(2)-i])
+    for k = 1:size(table,1);
+        if [pos(1)-i-1,pos(2)-i-1] == y (k,:)
+            Y = Y [pos(1)-i-1,pos(2)-i-1]
+        end
+    end
+    i =1;
+    while table(pos(1)+i,pos(2)-i) == 0
+        X = [X,[pos(1)+i,pos(2)-i]];
+        i=i+1;
+    end
+    y = square_check(table, [pos(1)+i,pos(2)-i])
+    for k = 1:size(table,1);
+        if [pos(1)+i+1,pos(2)-i-1] == y (k,:)
+            Y = [Y ,[pos(1)+i+1,pos(2)-i-1]]
+        end
+    end
+    i =1;
+    while table(pos(1)+i,pos(2)+i) == 0
+        X = [X,[pos(1)+i,pos(2)+i]];
+        i=i+1;
+    end
+     y = square_check(table, [pos(1)+i,pos(2)+i])
+    for k = 1:size(table,1);
+        if [pos(1)+i+1,pos(2)+i+1] == y (k,:)
+            Y = [Y, [pos(1)+i-1,pos(2)+i+1]]
+        end
+    end
+    i =1;
+    while table(pos(1)-i,pos(2)+i) == 0
+        X = [X,[pos(1)-i,pos(2)+i]];
+        i=i+1;
+    end
+       y = square_check(table, [pos(1)+i,pos(2)+i])
+    for k = 1:size(table,1);
+        if [pos(1)-i-1,pos(2)+i+1] == y (k,:)
+            Y = [Y [pos(1)-i-1,pos(2)+i+1]]
+        end
+    end
+    i = 1;
+    X(1,:)=[];
+    
+    
+end
+
